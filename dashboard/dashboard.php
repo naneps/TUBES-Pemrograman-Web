9ea09568aed783c5b75f30e1934d7dc5b57ebc7f
Andditafsal <ekaprasetya2244@gmail.com>
2021-06-06 22:38:10 +0700

    Progress

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+if (!$_SESSION['id_user']) {
+  header('location:login/login.php');
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +15,7 @@ session_start();
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/form.css">
   <link rel="stylesheet" href="css/tabel.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+  <script src="https://kit.fontawesome.com/fc4d05615c.js" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
 </head>
 
@@ -25,7 +28,7 @@ session_start();
       <i class="fas fa-bars" id="sidebar_btn"></i>
     </label>
     <div class="left_area">
-      <h3>Kelola </h3>
+      <h3>Kelola Data</h3>
     </div>
 
   </header>
@@ -38,8 +41,9 @@ session_start();
     </div>
     <div class="mobile_nav_items">
       <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
-      <a href="#"><i class="fas fa-cogs"></i><span>Components</span></a>
+      <a href="#"><i class="fab fa-wpforms"></i><span>Form</span></a>
       <a href="#"><i class="fas fa-table"></i><span>Tables</span></a>
+      <a href="../login/logout_action.php"><i class="fas fa-sign-out-alt"></i><span>Logout</span></a>
 
     </div>
   </div>
@@ -47,12 +51,13 @@ session_start();
   <!--sidebar start-->
   <div class="sidebar">
     <div class="profile_info">
-      <img src="" class="profile_image" alt="img">
-      <h4>...</h4>
+      <!-- <img src="" class="profile_image" alt="img"> -->
+      <h4><?= $_SESSION['nama'] ?></h4>
     </div>
     <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
-    <a href="#"><i class="fas fa-cogs"></i><span>Components</span></a>
+    <a href="#"><i class="fab fa-wpforms"></i></i><span>Form</span></a>
     <a href="#"><i class="fas fa-table"></i><span>Tables</span></a>
+    <a href="../login/logout_action.php"><i class="fas fa-sign-out-alt"></i><span>Logout</span></a>
     <!--sidebar end-->
   </div>
   <div class="content">
@@ -83,109 +88,106 @@ session_start();
         </tbody>
       </table>
     </div>
-    <div class=" card">
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-    </div>
-  </div>
 
-  <script type="text/javascript">
-    $(document).ready(function() {
-      $('.nav_btn').click(function() {
-        $('.mobile_nav_items').toggleClass('active');
+
+    <script type="text/javascript">
+      $(document).ready(function() {
+        $('.nav_btn').click(function() {
+          $('.mobile_nav_items').toggleClass('active');
+        });
       });
-    });
-  </script>
-  <script type="text/javascript">
-    function validasi() {
-      var menu = document.getElementById("menu").value;
-      var harga = document.getElementById("harga").value;
-      var detail = document.getElementById("detail").value;
-      var img = document.getElementById("img").value;
-      var msg = document.getElementById('alert');
+    </script>
+    <script type="text/javascript">
+      function validasi() {
+        var menu = document.getElementById("menu").value;
+        var harga = document.getElementById("harga").value;
+        var detail = document.getElementById("detail").value;
+        var img = document.getElementById("img").value;
+        var msg = document.getElementById('alert');
 
-      if (menu != "" && harga != "") {
+        if (menu != "" && harga != "") {
+          var xhttp = new XMLHttpRequest();
+          xhttp.open("POST", "../proses/ajaxfile.php", true);
+          xhttp.setRequestHeader("Content-Type", "application/json");
+          xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+              // Response
+              var response = this.responseText;
+              if (response == 1) {
+                alert("Insert successfully.");
+                loadEmployees();
+              }
+            }
+          };
+          var data = {
+            menu: menu,
+            harga: harga,
+            detail: detail,
+            img: img,
+          };
+          xhttp.send(JSON.stringify(data));
+        } else {
+          alert('Data Harus Diisi');
+          window.location = 'dashboard.php';
+          return false;
+        }
+      }
+
+      loadEmployees();
+
+      // Load records with GET request
+      function loadEmployees() {
         var xhttp = new XMLHttpRequest();
-        xhttp.open("POST", "../proses/ajaxfile.php", true);
-        xhttp.setRequestHeader("Content-Type", "application/json");
+
+        // Set GET method and ajax file path with parameter
+        xhttp.open("GET", "../proses/ajaxfile.php?request=1", true);
+
+        // Content-type
+        xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        // call on request changes state
         xhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
-            // Response
-            var response = this.responseText;
-            if (response == 1) {
-              alert("Insert successfully.");
-              loadEmployees();
+
+            // Parse this.responseText to JSON object
+            var response = JSON.parse(this.responseText);
+
+            // Select <table id='empTable'> <tbody>
+            var empTable =
+              document.getElementById("empTable").getElementsByTagName("tbody")[0];
+
+            // Empty the table <tbody>
+            empTable.innerHTML = "";
+
+            // Loop on response object
+            for (var key in response) {
+              if (response.hasOwnProperty(key)) {
+                var val = response[key];
+
+                // insert new row
+                var NewRow = empTable.insertRow(-1);
+                var no = NewRow.insertCell(0);
+                var menu = NewRow.insertCell(1);
+                var harga = NewRow.insertCell(2);
+                var detail = NewRow.insertCell(3);
+                var img = NewRow.insertCell(4);
+                var action = NewRow.insertCell(5);
+
+                no.innerHTML = val['no'];
+                menu.innerHTML = val['menu'];
+                harga.innerHTML = val['harga'];
+                detail.innerHTML = val['detail'];
+                img.innerHTML = val['img'];
+                action.innerHTML = '  <a class="btn_edit" href="">UPDATE</a>  <a class="btn_delete" href="">DELETE</a>';
+              }
             }
           }
         };
-        var data = {
-          menu: menu,
-          harga: harga,
-          detail: detail,
-          img: img,
-        };
-        xhttp.send(JSON.stringify(data));
-      } else {
-        alert('Data Harus Diisi');
-        window.location = 'dashboard.php';
-        return false;
+
+        // Send request
+        xhttp.send();
       }
-    }
-
-    loadEmployees();
-
-    // Load records with GET request
-    function loadEmployees() {
-      var xhttp = new XMLHttpRequest();
-
-      // Set GET method and ajax file path with parameter
-      xhttp.open("GET", "../proses/ajaxfile.php?request=1", true);
-
-      // Content-type
-      xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-      // call on request changes state
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-
-          // Parse this.responseText to JSON object
-          var response = JSON.parse(this.responseText);
-
-          // Select <table id='empTable'> <tbody>
-          var empTable =
-            document.getElementById("empTable").getElementsByTagName("tbody")[0];
-
-          // Empty the table <tbody>
-          empTable.innerHTML = "";
-
-          // Loop on response object
-          for (var key in response) {
-            if (response.hasOwnProperty(key)) {
-              var val = response[key];
-
-              // insert new row
-              var NewRow = empTable.insertRow(-1);
-              var no = NewRow.insertCell(0);
-              var menu = NewRow.insertCell(1);
-              var harga = NewRow.insertCell(2);
-              var detail = NewRow.insertCell(3);
-              var img = NewRow.insertCell(4);
-              var action = NewRow.insertCell(5);
-
-              no.innerHTML = val['no'];
-              menu.innerHTML = val['menu'];
-              harga.innerHTML = val['harga'];
-              detail.innerHTML = val['detail'];
-              img.innerHTML = val['img'];
-              action.innerHTML = '  <a class="btn_edit" href="">UPDATE</a>  <a class="btn_delete" href="">DELETE</a>';
-            }
-          }
-        }
-      };
-
-      // Send request
-      xhttp.send();
-    }
-  </script>
+    </script>
 
 </body>
 
